c74969dafbf66f52a9eeb7d17fb04d5a3699ff0a
Juha Rantanen <juha@codercoded.com>
2018-10-01 21:39:43 +0300

    Added basic gateway and updated Pipfile

---
+++ ruuvi_gateway.py
@@ -0,0 +1,26 @@
+from urllib.parse import quote
+from datetime import datetime
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+
+from google.cloud import firestore
+
+SCAN_TIMEOUT = 4
+
+db = firestore.Client()
+
+measurements_ref = db.collection(u'measurements')
+
+timestamp = datetime.now()
+
+def measurement_to_firestore(mac, payload):
+    result = payload.copy()
+    result["mac"] = mac
+    result["timestamp"] = timestamp
+    return result
+
+datas = RuuviTagSensor.get_data_for_sensors(None, SCAN_TIMEOUT)
+for mac, payload in datas.items():
+    value = measurement_to_firestore(mac, payload)
+    measurements_ref.add(value)
+    print('{0}: Measurement added for {1}'.format(timestamp.strftime('%Y-%m-%d %H:%M'), mac))
+
